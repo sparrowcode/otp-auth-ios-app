7142b05deefe95cc3854935cf7ad1014b321968b
Ivan Vorobei <hello@ivanvorobei.io>
2023-04-24 16:40:52 +0300

    Fixed swipe action bug.

--- iOS App/Scenes/Home/HomeController+Delegate.swift
+++ iOS App/Scenes/Home/HomeController+Delegate.swift
@@ -6,64 +6,63 @@ import NativeUIKit
 extension HomeController: SPDiffableTableDelegate, SPDiffableTableMediator {
     
     func diffableTableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        true
+        if indexPath.section == 0 && !passwordsData.isEmpty {
+            return true
+        }
+        return false
     }
     
     func diffableTableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForItem item: SPDiffableItem, at indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
-        let modelID = item.id
-        if indexPath.section == 0 {
-            
-            if !passwordsData.isEmpty {
-                
-                let actionExport = UIContextualAction(style: .normal, title: Texts.Shared.export) { [weak self] action, view, completion in
-                    guard let self = self else { return }
-                    let link = self.passwordsData[indexPath.row].url
-                    let controller = ExportController(link: link.absoluteString)
-                    let navigationController = NativeNavigationController(rootViewController: controller)
-                    self.present(navigationController, animated: true)
-                    completion(true)
-                }
-                
-                actionExport.backgroundColor = .systemIndigo
-                actionExport.image = Images.export
-                
-                let actionDelete = UIContextualAction(style: .destructive, title: Texts.Shared.delete) { [weak self] action, view, completion in
-                    guard let self = self else { return }
-                    
-                    let alert = UIAlertController(
-                        title: Texts.HomeController.delete_alert_title,
-                        message: Texts.HomeController.delete_alert_message,
-                        preferredStyle: .actionSheet
-                    )
-                    let delete = UIAlertAction(
-                        title: Texts.Shared.delete,
-                        style: .destructive) { alert in
-                            if let url = URL(string: modelID) {
-                                self.passwordsData.remove(at: indexPath.row)
-                                KeychainStorage.delete(urls: [url])
-                                AlertService.code_deleted()
-                            }
-                        }
-                    let cancel = UIAlertAction(
-                        title: Texts.Shared.cancel,
-                        style: .cancel,
-                        handler: nil
-                    )
-                    
-                    alert.addAction(delete)
-                    alert.addAction(cancel)
-                    
-                    self.present(alert, animated: true, completion: nil)
-                    completion(true)
-                }
-                
-                actionDelete.image = Images.delete
-                
-                return UISwipeActionsConfiguration(actions: [actionDelete, actionExport])
-            }
+        if indexPath.section == 0 && !passwordsData.isEmpty {
+            return nil
         }
         
-        return nil
+        let modelID = item.id
+        let actionExport = UIContextualAction(style: .normal, title: Texts.Shared.export) { [weak self] action, view, completion in
+            guard let self = self else { return }
+            let link = self.passwordsData[indexPath.row].url
+            let controller = ExportController(link: link.absoluteString)
+            let navigationController = NativeNavigationController(rootViewController: controller)
+            self.present(navigationController, animated: true)
+            completion(true)
+        }
+        
+        actionExport.backgroundColor = .systemIndigo
+        actionExport.image = Images.export
+        
+        let actionDelete = UIContextualAction(style: .destructive, title: Texts.Shared.delete) { [weak self] action, view, completion in
+            guard let self = self else { return }
+            
+            let alert = UIAlertController(
+                title: Texts.HomeController.delete_alert_title,
+                message: Texts.HomeController.delete_alert_message,
+                preferredStyle: .actionSheet
+            )
+            let delete = UIAlertAction(
+                title: Texts.Shared.delete,
+                style: .destructive) { alert in
+                    if let url = URL(string: modelID) {
+                        self.passwordsData.remove(at: indexPath.row)
+                        KeychainStorage.delete(urls: [url])
+                        AlertService.code_deleted()
+                    }
+                }
+            let cancel = UIAlertAction(
+                title: Texts.Shared.cancel,
+                style: .cancel,
+                handler: nil
+            )
+            
+            alert.addAction(delete)
+            alert.addAction(cancel)
+            
+            self.present(alert, animated: true, completion: nil)
+            completion(true)
+        }
+        
+        actionDelete.image = Images.delete
+        
+        return UISwipeActionsConfiguration(actions: [actionDelete, actionExport])
     }
 }
